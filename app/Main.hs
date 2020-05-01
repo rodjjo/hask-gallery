0bdee2a24c18128268eeb883eda5fb891b76c712
rodrigo a. araujo <rodjjo@gmail.com>
2020-05-01 14:03:32 -0300

    Parse command line args

--- app/Main.hs
+++ app/Main.hs
@@ -6,6 +6,27 @@ import Network.Wai
 import Network.Wai.Handler.Warp
 import Servant
 import Servant.API
+import System.Console.ParseArgs
+import Data.Maybe
+
+data Options =
+    OptionCommand |
+    OptionPort
+    deriving (Ord, Eq, Show)
+
+argd :: [ Arg Options ]
+argd = [
+        Arg {argIndex = OptionPort,
+            argName = Just "port",
+            argAbbr = Just 'p',
+            argData = argDataDefaulted "port" ArgtypeInt 8080,
+            argDesc = "The port to listen at. ex. 8080"},
+        Arg { argIndex = OptionCommand,
+            argName = Nothing,
+            argAbbr = Nothing,
+            argData = argDataRequired "command" ArgtypeString,
+            argDesc = "Command to run: runserver|refresh"}
+    ]
 
 server :: Server Gallery
 server = hello :<|> user
@@ -16,6 +37,30 @@ server = hello :<|> user
 app :: Application
 app = serve gallery server
 
+
+runserver :: Int -> IO ()
+runserver port = do
+    putStrLn("Running server at port " ++ (show port) ++ "\nPress CTRL-C to terminate")
+    run port app
+
+refresh :: IO ()
+refresh = do
+    putStrLn("Did nothing yet")
+
+defaultPort :: Int
+defaultPort = 8080
+
 main :: IO ()
 main = do
-    run 8080 app
+    args <- parseArgsIO (ArgsParseControl ArgsComplete ArgsSoftDash) argd
+    let command = case (getArgString args OptionCommand) of
+                Just s -> (s ::String)
+                Nothing -> ("" ::String)
+
+    let port = case (getArgInt args OptionPort) of
+                Just d -> (d ::Int)
+                Nothing -> defaultPort
+    case () of
+        ()  | command == ("runserver" ::String) -> runserver port
+            | command == ("refresh" ::String) -> refresh
+            | otherwise -> putStrLn("Unknow command " ++ command ++ "\n" ++ argsUsage args)
