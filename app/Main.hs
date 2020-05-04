aa497cace6a94c7c554dec2f57c3986c20cd6781
rodrigo a. araujo <rodjjo@gmail.com>
2020-05-04 20:41:41 -0300

    Simplify management commands and add .gitattributes

--- app/Main.hs
+++ app/Main.hs
@@ -5,12 +5,10 @@
 module Main where
 
 import Gallery
-import qualified Models.Video as VM
-import qualified Models.Settings as SM
+import qualified Management.Command as MC
 import Control.Applicative (Alternative((<|>)))
-import Data.Eq (Eq)
 import Data.Int (Int)
-import Data.List ((++), elem, drop, take)
+import Data.List ((++), drop)
 import Data.Maybe
 import Data.Ord (Ord)
 import Data.String (String)
@@ -21,33 +19,7 @@ import Servant
 import Servant.API
 import System.Console.ParseArgs
 import System.IO (IO, putStrLn)
-import Text.Show (Show, show)
-
----------------------------------------------------------------------------------------------------
-data Options =
-    OptionCommand |
-    OptionPort |
-    OptionText
-    deriving (Ord, Eq, Show)
-
-argd :: [ Arg Options ]
-argd = [
-        Arg {argIndex = OptionPort,
-            argName = Just "port",
-            argAbbr = Just 'p',
-            argData = argDataDefaulted "port" ArgtypeInt 8080,
-            argDesc = "The port to listen at. ex. 8080"},
-        Arg { argIndex = OptionCommand,
-            argName = Nothing,
-            argAbbr = Nothing,
-            argData = argDataRequired "command" ArgtypeString,
-            argDesc = "Command to run: runserver|refresh|gal-title|gal-videos-path"},
-        Arg { argIndex = OptionText,
-            argName = Nothing,
-            argAbbr = Nothing,
-            argData = argDataDefaulted "text" ArgtypeString "",
-            argDesc = "Text parameter for gal-title or gal-*-path command"}
-    ]
+import Text.Show (show)
 
 ---------------------------------------------------------------------------------------------------
 server :: Server Gallery
@@ -61,32 +33,12 @@ app :: Application
 app = serve gallery server
 
 ---------------------------------------------------------------------------------------------------
-runserver :: Int -> IO ()
-runserver port = do
-    putStrLn("Running server at port " ++ (show port) ++ "\nPress CTRL-C to terminate")
-    run port app
-
----------------------------------------------------------------------------------------------------
-setSetting :: String -> String -> IO ()
-setSetting option text = do
-    settings <- SM.load
-    SM.save $ (SM.optionSetter $ drop 4 option) settings text
-
----------------------------------------------------------------------------------------------------
-adaptCommand :: String -> String
-adaptCommand text
-    | take 4 text == "gal-" = "gal-config"
-    | otherwise = text
+runServer :: Maybe Int -> IO ()
+runServer port = do
+    let serverPort = fromJust $ port <|> Just 8080 -- TODO(rodrigo): falback port to an environment variable
+    putStrLn("Running server at port " ++ (show serverPort) ++ "\nPress CTRL-C to terminate")
+    run serverPort app
 
 ---------------------------------------------------------------------------------------------------
 main :: IO ()
-main = do
-    args <- parseArgsIO (ArgsParseControl ArgsComplete ArgsSoftDash) argd
-    let command = fromJust $ getArgString args OptionCommand <|> Just ""
-    let text = fromJust $ getArgString args OptionText <|> Just ""
-    let port = fromJust $ getArgInt args OptionPort  <|> Just (8080 ::Int)
-    case (adaptCommand command) of
-        "runserver" -> runserver port
-        "refresh" -> VM.updateCache
-        "gal-config" -> setSetting command text
-        _ -> putStrLn("Unknow command " ++ command ++ "\n" ++ argsUsage args)
+main = MC.exec runServer
---
+++ src/Management/Command.hs
@@ -0,0 +1,75 @@
+module Management.Command (
+        exec
+    ) where
+
+import qualified Models.Video as VM
+import qualified Models.Settings as SM
+
+import Control.Applicative (Alternative((<|>)))
+import Data.Eq (Eq)
+import Data.Int (Int)
+import Data.List (elem, foldl, drop, take, (++))
+import Data.Maybe
+import Data.Ord (Ord)
+import Data.String (String)
+import Prelude (otherwise, return, (==), (||), ($))
+import qualified System.Console.ParseArgs as PA
+import System.IO (IO, putStrLn)
+import Text.Show (Show)
+
+---------------------------------------------------------------------------------------------------
+data Options =
+    OptionCommand |
+    OptionPort |
+    OptionText
+    deriving (Ord, Eq, Show)
+
+commands :: [String]
+commands = ["runserver", "refresh", "gal-title", "gal-videos-path"]
+
+argd :: [ PA.Arg Options ]
+argd = [
+        PA.Arg {
+            PA.argIndex = OptionPort,
+            PA.argName = Just "port",
+            PA.argAbbr = Just 'p',
+            PA.argData = PA.argDataOptional "port" PA.ArgtypeInt,
+            PA.argDesc = "The port to listen at. ex. 8080"},
+        PA.Arg {
+            PA.argIndex = OptionCommand,
+            PA.argName = Nothing,
+            PA.argAbbr = Nothing,
+            PA.argData = PA.argDataRequired "command" PA.ArgtypeString,
+            PA.argDesc = "Command: " ++ foldl (\a b -> a ++ b ++ "|") "|" commands },
+        PA.Arg {
+            PA.argIndex = OptionText,
+            PA.argName = Nothing,
+            PA.argAbbr = Nothing,
+            PA.argData = PA.argDataDefaulted "text" PA.ArgtypeString "",
+            PA.argDesc = "Text parameter for gal-title or gal-*-path command"}
+    ]
+
+---------------------------------------------------------------------------------------------------
+adaptCommand :: String -> String
+adaptCommand text
+    | take 4 text == "gal-" = "gal-config"
+    | otherwise = text
+
+---------------------------------------------------------------------------------------------------
+setSetting :: String -> String -> IO ()
+setSetting option text = do
+    settings <- SM.load
+    SM.save $ (SM.optionSetter $ drop 4 option) settings text
+
+---------------------------------------------------------------------------------------------------
+exec :: (Maybe Int -> IO ()) -> IO ()
+exec runServer = do
+    args <- PA.parseArgsIO (PA.ArgsParseControl PA.ArgsComplete PA.ArgsSoftDash) argd
+    let command = fromJust $ PA.getArgString args OptionCommand <|> Just ""
+    let text = fromJust $ PA.getArgString args OptionText <|> Just ""
+    let port = PA.getArgInt args OptionPort
+    case (adaptCommand command) of
+        "runserver" -> runServer port
+        "gal-config" -> setSetting command text
+        "refresh" -> VM.updateCache
+        _ -> putStrLn ("An unknow command was entered: " ++ command ++ "\n" ++ (PA.argsUsage args))
