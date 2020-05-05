7c1e53de72ba021c5706f99fb8980bb25b43af5c
rodrigo a. araujo <rodjjo@gmail.com>
2020-05-05 00:11:39 -0300

    Start the mediafile proble functions

---
+++ src/Probe/FFProbe.hs
@@ -0,0 +1,132 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Probe.FFProbe (
+         probeInstalled
+        ,probeFile
+        --,getDimensions
+        --,getDuration
+        --,getMediaType
+        --,getModifiedAt
+        --,getCreatedAt
+        ,Mediatype(..)
+    ) where
+
+import Data.Eq (Eq)
+import Data.List.NonEmpty (NonEmpty)
+import Data.List (foldl)
+import Data.List.Split (endBy)
+import Data.Int (Int)
+import Data.Maybe
+import Data.String (String)
+import Data.Bool (Bool)
+import System.Directory ( getHomeDirectory )
+import System.Exit ( ExitCode(..) )
+import System.FilePath (FilePath)
+import System.IO (IO, putStrLn)
+import System.Process ( readProcessWithExitCode )
+import Prelude (otherwise, return, (==), (||), ($), (/=))
+
+--------------------------------------------------------------------------------------------------
+ffprobe :: String
+ffprobe = "ffprobe"
+
+--------------------------------------------------------------------------------------------------
+data Mediatype = MediaAudio|MediaVideo|MediaUnknow deriving Eq
+
+data MediaInfo = MediaInfo {
+         mediaType ::Mediatype
+        ,duration ::Int
+        ,modifiedAt ::Int
+        ,createdAt ::Int
+        ,dimensions :: (Int,Int)
+    }
+
+--------------------------------------------------------------------------------------------------
+ffprobeExec :: String -> IO (Maybe String)
+ffprobeExec filepath = do
+    (exitCode, stdOut, stdErr) <- readProcessWithExitCode (ffprobe) ["-i", filepath] ""
+    if exitCode /= ExitSuccess
+        then return Nothing
+        else return $ Just stdOut
+
+--------------------------------------------------------------------------------------------------
+dimensionsRegex :: String
+dimensionsRegex = "([0-9]{3,5}x[0-9]{3,5})"
+
+--------------------------------------------------------------------------------------------------
+durationRegex :: String
+durationRegex = "Duration:[ ]+([0-9]{2}:[0-9]{2}:[0-9]{2})"
+
+--------------------------------------------------------------------------------------------------
+parseDuration :: String -> Int
+parseDuration text = 0
+
+--------------------------------------------------------------------------------------------------
+parseDimensions :: Int -> Int -> String -> (Int, Int)
+parseDimensions width height text = (0, 0)
+
+--------------------------------------------------------------------------------------------------
+parseMediaType :: String -> Mediatype
+parseMediaType text = MediaUnknow
+
+--------------------------------------------------------------------------------------------------
+type ParseData = (Mediatype, Int, (Int, Int))
+emptyParseData :: ParseData
+emptyParseData = (MediaUnknow, 0, (0, 0))
+
+--------------------------------------------------------------------------------------------------
+parseNext :: ParseData  -> String -> ParseData
+parseNext (mType, duration, (width, height)) text = (
+         if mType == MediaUnknow
+            then parseMediaType text
+            else mType
+        ,if duration == 0
+            then parseDuration text
+            else duration
+        ,if (width == 0) || (height == 0)
+            then parseDimensions width height text
+            else (width, height)
+    )
+
+--------------------------------------------------------------------------------------------------
+parseAll :: [String] -> (Mediatype, Int, (Int, Int))
+parseAll contents = foldl (\last line -> parseNext last line) emptyParseData $ contents
+
+--------------------------------------------------------------------------------------------------
+parseInfo :: Int -> Int -> [String] -> MediaInfo
+parseInfo mCreatedAt mModifiedAt contents = MediaInfo {
+         mediaType=mType
+        ,duration=mDuration
+        ,modifiedAt=mModifiedAt
+        ,createdAt=mCreatedAt
+        ,dimensions=mDimensions
+    } where
+        (mType, mDuration, mDimensions) = parseAll contents
+
+--------------------------------------------------------------------------------------------------
+probeInstalled :: IO Bool
+probeInstalled = do
+    (exitCode, stdOut, stdErr) <- readProcessWithExitCode (ffprobe) ["--version"] ""
+    return $ exitCode == ExitSuccess
+
+--------------------------------------------------------------------------------------------------
+getModificationTime :: FilePath -> IO Int
+getModificationTime filepath = do
+    return 0
+
+--------------------------------------------------------------------------------------------------
+getCreationTime :: FilePath -> IO Int
+getCreationTime filepath = do
+    return 0
+
+--------------------------------------------------------------------------------------------------
+probeFile :: FilePath -> IO (Maybe MediaInfo)
+probeFile filepath = do
+    probedData <- ffprobeExec filepath
+    mCreatedAt <- getCreationTime filepath
+    mModifiedAt <- getModificationTime filepath
+    case probedData of
+        Just info -> return $ Just $ parseInfo mCreatedAt mModifiedAt $ endBy "\n" info
+        Nothing -> return Nothing
