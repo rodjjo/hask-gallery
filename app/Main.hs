0a3159cf24b86dc61b269a3d0b026ed16ad8e57d
Rodrigo Antônio de Araújo <rodjjo@gmail.com>
2020-05-08 01:46:37 -0300

    Structure endpoints (#7)

--- app/Main.hs
+++ app/Main.hs
@@ -4,41 +4,11 @@
 
 module Main where
 
-import Gallery
+import Application (runServer)
 import qualified Management.Command as MC
-import Control.Applicative (Alternative((<|>)))
-import Data.Int (Int)
-import Data.List ((++), drop)
-import Data.Maybe
-import Data.Ord (Ord)
-import Data.String (String)
-import Network.Wai
-import Network.Wai.Handler.Warp
-import Prelude (otherwise, return, (==), (||), ($))
-import Servant
-import Servant.API
-import System.Console.ParseArgs
-import System.IO (IO, putStrLn)
-import Text.Show (show)
+
+import System.IO (IO)
 import GHC.IO.Encoding (setLocaleEncoding, utf8)
-
----------------------------------------------------------------------------------------------------
-server :: Server Gallery
-server = hello :<|> user
-    where
-        hello = return "Hello world"
-        user n a = return $ User n a
-
----------------------------------------------------------------------------------------------------
-app :: Application
-app = serve gallery server
-
----------------------------------------------------------------------------------------------------
-runServer :: Maybe Int -> IO ()
-runServer port = do
-    let serverPort = fromJust $ port <|> Just 8080 -- TODO(rodrigo): falback port to an environment variable
-    putStrLn("Running server at port " ++ (show serverPort) ++ "\nPress CTRL-C to terminate")
-    run serverPort app
 
 ---------------------------------------------------------------------------------------------------
 main :: IO ()
---
+++ src/Routes.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators     #-}
+
+module Routes (
+         gallery
+        ,endpoints
+    ) where
+
+import qualified Views.HealthChecker as HC
+import Servant (Server(..), Proxy(..), JSON)
+import Servant.API  (Get(..), (:<|>)(..), (:>)(..))
+import Prelude (($), return)
+
+--------------------------------------------------------------------------------------------------
+type GalleryApi =
+    "health-check/liveness" :> HC.HealthCheck
+    :<|> "health-check/readiness" :> HC.HealthCheckReadiness
+
+--------------------------------------------------------------------------------------------------
+gallery :: Proxy GalleryApi
+gallery = Proxy
+
+--------------------------------------------------------------------------------------------------
+endpoints :: Server GalleryApi
+endpoints = a :<|> b
+    where
+        a = return $ HC.healthCheck
+        b = return $ HC.healthCheckReadiness
