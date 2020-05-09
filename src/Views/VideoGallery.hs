80d0e80e8cd4b83a5a4b17f4abd9dbf36fe91336
Rodrigo Antônio de Araújo <rodjjo@gmail.com>
2020-05-09 15:51:42 -0300

    Implement the endpoint that return the video list (#12)
    * Implement the base of videos endpoint
    
    * Add pagination support to pagination

--- src/Views/VideoGallery.hs
+++ src/Views/VideoGallery.hs
@@ -10,17 +10,24 @@ module Views.VideoGallery (
 
 import qualified Views.Base as VB
 import qualified Models.Video as MV
+import qualified Models.Base as MB
+import qualified Utils as UT
 
+import Control.Concurrent.STM.TVar (TVar, readTVar, writeTVar)
+import Control.Monad.IO.Class (liftIO)
+import Control.Monad.STM (atomically)
+import Control.Monad.Trans.Reader (ask)
 import Data.Aeson (FromJSON, ToJSON)
 import Data.Text  (Text)
 import Data.Int (Int)
 import Data.Eq (Eq)
+import Data.List (take, drop)
 import GHC.Generics (Generic)
 import Servant.API (Get)
 import Servant (JSON)
 import Text.Read (Read)
 import Text.Show (Show)
-import Prelude (return, ($))
+import Prelude (Double, ceiling, fromInteger, length, return, toInteger, ($), (>=), (/=), (*), (-), (/))
 import System.IO (IO)
 import Servant.Server.Internal.Handler (Handler(..))
 
@@ -29,7 +36,7 @@ import Servant.Server.Internal.Handler (Handler(..))
 data VideosPagination = VideosPagination
     { page :: Int
     , pageCount :: Int
-    , durationTotal :: Int
+    , maxItemsPerPage :: Int
     , randomSeed :: Int
     } deriving (Eq, Show, Generic)
 
@@ -37,16 +44,43 @@ instance FromJSON VideosPagination
 instance ToJSON VideosPagination
 
 data VideosPayload = VideosPayload
-    { videos :: MV.VideoList
+    { items :: MV.VideoList
+    , itemCount :: Int
+    , totalDuration :: Int
     , pagination :: VideosPagination
     } deriving (Eq, Show, Generic)
 instance FromJSON VideosPayload
 instance ToJSON VideosPayload
+
+
+---------------------------------------------------------------------------------------------------
+toDouble :: Int -> Double
+toDouble n1 = fromInteger $ toInteger n1
+
+---------------------------------------------------------------------------------------------------
+shuffleVideoList :: MV.VideoList -> Int -> MV.VideoList
+shuffleVideoList theList theSeed = MB.shuffleModelList theList theSeed
+
+---------------------------------------------------------------------------------------------------
+paginateVideoList ::  MV.VideoList -> Int -> Int -> Int -> IO (MV.VideoList, VideosPagination)
+paginateVideoList theList theSeed thePage thePageSize = do
+    let shuffledList = shuffleVideoList theList theSeed
+    let maxPages = ceiling ((toDouble (length shuffledList)) / (toDouble  thePageSize))
+    let finalPage = if thePage >= maxPages then maxPages - 1 else thePage
+    let pageItems = take thePageSize $ drop (finalPage * thePageSize) shuffledList
+    return (pageItems, VideosPagination finalPage maxPages thePageSize theSeed)
 
 ---------------------------------------------------------------------------------------------------
 type GetVideoList = Get '[JSON] VideosPayload
 
 getVideoList :: Int -> Int -> VB.GalleryMonad VideosPayload
 getVideoList seed page = do
-    let pa = VideosPagination 0 0 0 0
-    return $ VideosPayload [] pa
+    VB.State { VB.videos = p } <- ask
+    gallery <- liftIO $ atomically $ readTVar p
+    randomSeed <- if seed /= 0 then return seed else liftIO $ UT.getRandomSeed
+    ( shuffledList, pagination ) <- liftIO $ paginateVideoList (MV.getGalleryVideos gallery) randomSeed page 100
+    return VideosPayload { items = shuffledList
+                         , itemCount = MV.getGalleryCount gallery
+                         , totalDuration = MV.getGalleryDuration gallery
+                         , pagination = pagination
+                         }
