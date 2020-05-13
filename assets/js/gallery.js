3becc1858f5789751b0c9d6d87faefdb73e4111e
rodrigo a. araujo <rodjjo@gmail.com>
2020-05-12 21:29:54 -0300

    Remove frontend dependencies and add simplify it

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,175 +1,238 @@
-let current_index = 0;
+function GalleryModule() {
+    let currentIndex = 0;
+    let videos = [];
+    let currentPage = "";
 
-let videos = [];
-let currentSeed = 0;
-
-function dePaginate(page, previousVideos) {
-    $.get(`/videos/${currentSeed}/${page}`, function( data ) {
-        currentSeed = data.pagination.randomSeed;
-        if (data.pagination.page + 1 < data.pagination.pageCount) {
-            dePaginate(data.pagination.page + 1, previousVideos.concat(data.items));
-        } else {
-            videos = previousVideos.concat(data.items);
+    const pageControls = {
+        "videos": {
+            "controls": ["id-video-page", "id-bottom-panel-video"],
+            "order": 0,
+            "toggleControl": "id-toggle-video",
+            "streamingControl": "id-video",
+            "topBarControl": "id-top-panel",
+            "bottomBarControl": "id-bottom-panel-video",
+        },
+        "music": {
+            "controls": ["id-music-page", "id-bottom-panel-music"],
+            "order": 1,
+            "toggleControl": "id-toggle-music",
+            "topBarControl": "id-top-panel",
+            "bottomBarControl": "id-bottom-panel-video",
+        },
+        "pictures": {
+            "controls": ["id-picture-page", "id-bottom-panel-picture"],
+            "order": 1,
+            "topBarControl": "id-top-panel",
+            "toggleControl": "id-toggle-picture",
         }
-    });
-}
+    };
 
-function toggle_random(value, handler) {
-   // not implemented yet
-}
+    const doc = document;
+    const de = doc.documentElement;
+    const fullScreenTogleFunctions = [
+        ( doc.cancelFullScreen || doc.mozCancelFullScreen || doc.webkitCancelFullScreen || (() => {})),
+        ( de.requestFullscreen || de.mozRequestFullScreen || (
+                () => {
+                    if (de.webkitRequestFullscreen) de.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT)
+                }))
+    ];
 
-function change_video(data) {
-    let component = $('#id-video');
-    component.attr('src', `/files/videos${encodeURI(data.videoPath).replace(/#/g, '%23')}`);
+    const IsFullScreen = () => (!(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement));
 
-    let content_info = $('#content-info');
-    content_info.text(data.videoPath);
-    let page_info = $('#page-info');
-    page_info.text(`${current_index + 1} of ${videos.length}`);
+    const format = function() {
+        const args = arguments;
+        return this.replace(/{(\d+)}/g, function(match, number) {
+            return typeof args[number] != 'undefined'
+                ? args[number]
+                : match
+            ;
+        });
+    };
 
-    try {
-        component[0].load();
-        component[0].play();
-    } catch (error) {
-        console.log('---- not user');
-    }
-}
-
-function next_video() {
-    current_index += 1;
-    display_current();
-}
-
-
-function prior_video() {
-    if (current_index == 0) {
-        return;
-    }
-    current_index -= 1;
-    display_current();
-}
-
-function displayControls(x, y, max_y) {
-    let top_panel = $("#id-top-panel-video");
-    let bottom_panel = $("#id-bottom-panel-video");
-    if (y < max_y * 0.25) {
-        if (top_panel.css('display') !== "block") {
-            top_panel.css('display', "block");
-        }
-    } else {
-        if (top_panel.css('display') !== "none") {
-            top_panel.css('display', "none");
+    function get(template) {
+        return function() {
+            const args = Array.from(arguments).slice(0, -1);
+            const callback = Array.from(arguments).slice(-1)[0];
+            const url = format.apply(template, args);
+            $.get(url, callback);
         }
     }
-    if (y > max_y * 0.75) {
-        if (bottom_panel.css('display') !== "block") {
-            bottom_panel.css('display', "block");
-        }
-    } else {
-        if (bottom_panel.css('display') !== "none") {
-            bottom_panel.css('display', "none");
+
+    function composeURI(base) {
+        return function(path) {
+            return `${base}${encodeURI(path).replace(/#/g, '%23')}`
         }
     }
-}
 
-function mouse_move_handler (e) {
-    let component = $(this);
-    let parent_coords = component.offset();
-    let x = e.pageX - parent_coords.left;
-    let y = e.pageY - parent_coords.top;
+    const videoFilesURL = composeURI('/files/videos');
+    const songsFilesURL = composeURI('/files/songs');
+    const pictureFilesURL = composeURI('/files/pictures');
 
-    displayControls(x, y, component.height());
-}
+    const getVideos = get('/videos/{0}/{1}/');
+    const getSongs = get('/songs/{0}/{1}/');
+    const getPictures = get('/pictures/{0}/{1}/');
 
-function goFullScreen() {
-    let element = document.body;
-    // Supports most browsers and their versions.
-    let requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
+    function updateVideoInfo(data) {
+        $('#content-info').text(data.videoPath);
+        $('#id-video-page-info').text(`${currentIndex + 1} of ${videos.length} videos`);
+        $('#id-video-seekbar')[0].time = 0;
+    }
 
-    if (requestMethod) { // Native full screen.
-        requestMethod.call(element);
-    } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
-        let wscript = new ActiveXObject("WScript.Shell");
-        if (wscript !== null) {
-            wscript.SendKeys("{F11}");
+    function changeVideo(data) {
+        const videoComponent = $('#id-video');
+        videoComponent.attr('src', videoFilesURL(data.videoPath));
+        videoComponent[0].load();
+        videoComponent[0].play();
+    }
+
+    function nextVideo() {
+        currentIndex += 1;
+        displayVideoAtCurrentIndex();
+    }
+
+
+    function previousVideo() {
+        if (currentIndex > 0) {
+            currentIndex -= 1;
+            displayVideoAtCurrentIndex();
         }
     }
-}
 
-function viewer_click(e) {
-    let component = $(this);
-    let parent_coords = component.offset();
-    let x = e.pageX - parent_coords.left;
-    let y = e.pageY - parent_coords.top;
-    let video = $("#id-video")[0];
-
-    if (y >= component.height() - 85) {
-        return;
+    function showPage(pageName) {
+        currentPage = pageName;
+        Object.keys(pageControls).forEach((name) => {
+            const page = pageControls[name];
+            const isCurrentPage = (name === pageName);
+            const display = isCurrentPage ? "block": "None";
+            const streamingFunction = isCurrentPage ? "play" : "pause";
+            page.controls.forEach((controlId) => {
+                $(`#${controlId}`).css("display", display);
+            });
+            if (page.streamingControl)
+                ($(`#${page.streamingControl}`)[0][streamingFunction])();
+        });
     }
 
-    if (y <= component.height() / 4 && x >= component.width() - component.width() / 4) {
+    function displayControls(x, y, max_y) {
+        const topBarDisplay = y < max_y * 0.25 ? "block" : "none";
+        const bottomBarDisplay = y > max_y * 0.75 ? "block" : "none";
+        const page = pageControls[currentPage];
+        if (page.topBarControl)
+            $(`#${page.topBarControl}`).css("display", topBarDisplay);
+        if (page.bottomBarControl)
+            $(`#${page.bottomBarControl}`).css("display", bottomBarDisplay);
+    }
+
+    function handleFrontPanelMouseMove (e) {
+        let component = $(this);
+        let parent_coords = component.offset();
+        let x = e.pageX - parent_coords.left;
+        let y = e.pageY - parent_coords.top;
+
+        displayControls(x, y, component.height());
+    }
+
+    const toggleFullScreen = () => {
+        (fullScreenTogleFunctions[IsFullScreen() ? 0 : 1]).apply(IsFullScreen() ? doc : de);
+    }
+
+    function handleFrontPanelClick(e) {
+        const page = pageControls[currentPage];
+        const component = $(this);
+        const parentCoords = component.offset();
+        const x = e.pageX - parentCoords.left;
+        const y = e.pageY - parentCoords.top;
+        const maxClickableY = page.bottomBarControl ? $( window ).height() - $(`#${page.bottomBarControl}`).height() : 1000000000;
+        const minClickableY = page.topBarControl ? $(`#${page.topBarControl}`).height() : 0;
+
+        if (maxClickableY < y || y < minClickableY)
+            return;
+
         e.preventDefault();
-        goFullScreen();
-        return;
-    }
 
-    if (x > (component.width() / 3) * 2) {
-        e.preventDefault();
-        next_video();
-    } else if (x < component.width() / 3) {
-        e.preventDefault();
-        prior_video();
-    } else {
-        if (video.paused)
-            video.play();
-        else
-            video.pause();
-    }
-}
-
-function display_current() {
-    if (current_index >= videos.length) {
-        current_index = 0;
-    }
-    if (videos.length < 1) {
-        setTimeout(display_current, 2000);
-    } else {
-        change_video(videos[current_index]);
-    }
-}
-
-function sec2time(timeInSeconds) {
-    var pad = function(num, size) { return ('000' + num).slice(size * -1); },
-    time = parseFloat(timeInSeconds).toFixed(3),
-    hours = Math.floor(time / 60 / 60),
-    minutes = Math.floor(time / 60) % 60,
-    seconds = Math.floor(time - minutes * 60),
-    milliseconds = time.slice(-3);
-
-    return pad(hours, 2) + ':' + pad(minutes, 2) + ':' + pad(seconds, 2);
-}
-
-function EnableSeekBar() {
-    const video = $('#id-video')[0];
-    const seekbar = $('#id-seekbar')[0];
-
-    video.addEventListener('timeupdate', () => {
-        if (!video.seeking) {
-            seekbar.value = video.currentTime / video.duration * seekbar.max
+        if (y <= component.height() / 4 && x >= component.width() - component.width() / 4) {
+            toggleFullScreen();
+            return;
         }
-        $("#id-current-time").text(sec2time(Math.floor(video.currentTime)) + "/" + sec2time(Math.floor(video.duration))); //Change #current to currentTime
-    });
 
-    seekbar.addEventListener('change', () => {
-        video.currentTime = video.duration * seekbar.value / seekbar.max;
-    });
-}
+        if (currentPage === "videos") {
+            let video = $("#id-video")[0];
+            if (x > (component.width() / 3) * 2) {
+                nextVideo();
+            } else if (x < component.width() / 3) {
+                previousVideo();
+            } else {
+                if (video.paused)
+                    video.play();
+                else
+                    video.pause();
+            }
+        }
+    }
 
-$(document).ready(function(){
-    EnableSeekBar();
-    $("#id-videos-div").mouseup(viewer_click);
-    $("#id-videos-div").mousemove(mouse_move_handler);
-    videos = []
-    dePaginate(0, []);
-    display_current();
-});
+    function validateCurrentIndex() {
+        if (currentIndex >= videos.length)
+            currentIndex = 0;
+    }
+
+    function displayVideoAtCurrentIndex() {
+        validateCurrentIndex();
+        updateVideoInfo(videos[currentIndex])
+        changeVideo(videos[currentIndex]);
+    }
+
+    function sec2time(timeInSeconds) {
+        if (Number.isNaN(timeInSeconds))
+            return "00:00:00";
+        let result = (
+            pad = function(num, size) { return ('000' + num).slice(size * -1); }
+            , time = Math.round(timeInSeconds)
+            , hours = Math.floor(time / 60 / 60)
+            , minutes = Math.floor(time / 60) % 60
+            , seconds = Math.floor(time - minutes * 60)
+            , pad(hours, 2) + ':' + pad(minutes, 2) + ':' + pad(seconds, 2)
+        );
+        return result;
+    }
+
+    (function enableSeekBar() {
+        const video = $('#id-video')[0];
+        const seekbar = $('#id-video-seekbar')[0];
+        video.addEventListener('ended', () => {nextVideo()}, false);
+        video.addEventListener('timeupdate', () => {
+            if (!video.seeking) {
+                seekbar.value = video.currentTime / video.duration * seekbar.max
+            }
+            $("#id-video-time").text(sec2time(Math.floor(video.currentTime)) + "/" + sec2time(Math.floor(video.duration))); //Change #current to currentTime
+        });
+        seekbar.addEventListener('change', () => {
+            video.currentTime = video.duration * seekbar.value / seekbar.max;
+        });
+    }) ();
+
+    (function enablePageClicks() {
+        Object.keys(pageControls).forEach((pageName) => {
+            $(`#${pageControls[pageName].toggleControl}`).click(() => {
+                showPage(pageName);
+            });
+        });
+    }) ();
+
+    (function enableMouseHandling() {
+        $("#id-front-panel").mouseup(handleFrontPanelClick);
+        $("#id-front-panel").mousemove(handleFrontPanelMouseMove);
+    }) ();
+
+    (function dePaginate(currentSeed, page, currentList, resultCb) {
+        getVideos(currentSeed, page, function(data) {
+            if (data.pagination.page + 1 < data.pagination.pageCount)
+                dePaginate(data.pagination.randomSeed, data.pagination.page + 1, currentList.concat(data.items), resultCb);
+            else
+                resultCb(currentList.concat(data.items))
+        });
+    }) (0, 0, [], function(result) {videos=result; displayVideoAtCurrentIndex();});
+
+    showPage("videos");
+};
+
+$(document).ready(GalleryModule());
